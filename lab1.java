55a24e2f31437c5af8f1fe13a4179fbd37075c60
marta <momotko08@gmail.com>
2022-08-31 16:42:52 +0100

    Version with keyboard input

--- lab1.java
+++ lab1.java
@@ -6,18 +6,12 @@ public class lab1 {
         ArrayList<fibonacci> fiboList = new ArrayList<>();
         int N, digit;
 
-        if (args.length > 0){
-            N = Integer.parseInt(args[0]);
-            digit = Integer.parseInt(args[1]);
-        }
-        else{
-            Scanner scan = new Scanner(System.in);
-            System.out.print(" Enter how many first numbers would be taken: ");
-            N = scan.nextInt();
-            System.out.print(" Enter on which number fibonacci should end: ");
-            digit= scan.nextInt();
-            scan.close();
-        }
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Enter how many first numbers would be taken: ");
+        N = scan.nextInt();
+        System.out.print(" Enter on which number fibonacci should end: ");
+        digit= scan.nextInt();
+        scan.close();
 
         for(int i=1, j=1, n=1; n<=N; n++){
             fibonacci Obj = new fibonacci(n, i);
